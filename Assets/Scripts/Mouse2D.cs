3cce9383c975369bb2c007f565432e8d5d3f7ccb
GABE Lewis <gl267741@falmouth.ac.uk>
2021-10-18 16:28:04 +0100

    Fixed Mouse Ashley

--- Assets/Scripts/Mouse2D.cs
+++ Assets/Scripts/Mouse2D.cs
@@ -11,24 +11,19 @@ public class Mouse2D : MonoBehaviour
 
     private void Start()
     {
-        Cursor.visible = true;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        //RaycastHit hit;
-        //Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, -1, QueryTriggerInteraction.Ignore);
-        //Vector3 mouseWorldPosition = hit.point;
-        Vector2 inp = Input.mousePosition;
-        inp.x /= Screen.width;
-        inp.y /= Screen.height;
-        Vector3 mouseWorldPosition = new Vector3(
-            Mathf.Lerp(lowerBounds.x, upperBounds.x, inp.x),
-            Mathf.Lerp(lowerBounds.y, upperBounds.y, inp.y),
-            0);
-
+        
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPosition.z = 0f;
+
+        print(Input.mousePosition + ";;;" + mouseWorldPosition);
+             
         transform.position = mouseWorldPosition;
+
     }
 
 }
